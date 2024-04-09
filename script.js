f283e1f7dae018a8d5102ea6630a674c9ff2d20d
joshjuanzo <164358064+joshjuanzo@users.noreply.github.com>
2024-04-09 18:01:58 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function onPageLoad() {
+    console.log('Page loaded');
+}
+function onParagraphMouseOver() {
+    console.log('Mouse over paragraph');
+}
+
+function onParagraphMouseOut() {
+    console.log('Mouse out of paragraph');
+}
+
+function onImageMouseOver() {
+    console.log('Mouse over image');
+}
+
+function onImageMouseOut() {
+    console.log('Mouse out of image');
+}
+
