667befa21eff71965ad0b758d493fa4d6b8397c8
joshjuanzo <164358064+joshjuanzo@users.noreply.github.com>
2024-04-23 20:06:44 +0000

    update script j.s and debug some errors like curly bracket line

--- script.js
+++ script.js
@@ -48,22 +48,36 @@ document.getElementById("signup-form").addEventListener("submit", function(event
   var formData = {zipInput: zipInput, 
                   phoneInput: phoneInput
                  };
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
 
-    import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+document.getElementById("signup-form").addEventListener("submit", function(event) {
+  var zipInput = document.getElementById("zip").value;
+  var phoneInput = document.getElementById("phone").value;
+  var zipPattern = /^\d{5}(-\d{4})?$/;
+  var phonePattern = /^\d{3}-\d{3}-\d{4}$/;
 
-    const firebaseConfig = {
-      apiKey: "AIzaSyB4bQHGKOnq-IjW4r3ooYlnqD7j4Sie3rY",
-      authDomain: "aiwebsite-1cdfa.firebaseapp.com",
-      projectId: "aiwebsite-1cdfa",
-      storageBucket: "aiwebsite-1cdfa.appspot.com",
-      messagingSenderId: "434561316894",
-      appId: "1:434561316894:web:f33f53f73c21777fd8960f"
-    };
+  if (!zipPattern.test(zipInput) || !phonePattern.test(phoneInput)) {
+    alert("Please enter valid ZIP code and phone number.");
+    event.preventDefault();
 
-  const app = initializeApp(firebaseConfig);
+    var formData = {zipInput: zipInput, 
+                  phoneInput: phoneInput
+                 };
+  }
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyB4bQHGKOnq-IjW4r3ooYlnqD7j4Sie3rY",
+    authDomain: "aiwebsite-1cdfa.firebaseapp.com",
+    projectId: "aiwebsite-1cdfa",
+    storageBucket: "aiwebsite-1cdfa.appspot.com",
+    messagingSenderId: "434561316894",
+    appId: "1:434561316894:web:f33f53f73c21777fd8960f"
+  };
+
+  initializeApp(firebaseConfig);
+
   var formJSON = JSON.stringify(formData);
   console.log(formJSON);
   return formJSON;
-  }
-}
-);
+});
+    
