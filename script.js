5e9c6d97859beedc1184764896c04a0289f09fc3
joshjuanzo <164358064+joshjuanzo@users.noreply.github.com>
2024-04-23 18:09:20 +0000

    consolidated java scripts for login, sign up and index

--- script.js
+++ script.js
@@ -16,4 +16,41 @@ function onImageMouseOver() {
 function onImageMouseOut() {
     console.log('Mouse out of image');
 }
+document.addEventListener("DOMContentLoaded", function() {
+    var form = document.getElementById("login-form");
 
+    form.addEventListener("submit", function(event) {
+        event.preventDefault(); 
+
+        var username = document.getElementById("username").value;
+        var password = document.getElementById("password").value;
+
+
+        if (username === "user" && password === "password") {
+            alert("Login successful!");
+
+        } else {
+            alert("Invalid username or password. Please try again.");
+        }
+    });
+});
+
+document.getElementById("signup-form").addEventListener("submit", function(event) {
+  var zipInput = document.getElementById("zip").value;
+  var phoneInput = document.getElementById("phone").value;
+  var zipPattern = /^\d{5}(-\d{4})?$/;
+  var phonePattern = /^\d{3}-\d{3}-\d{4}$/;
+
+  if (!zipPattern.test(zipInput) || !phonePattern.test(phoneInput)) {
+    alert("Please enter valid ZIP code and phone number.");
+    event.preventDefault();
+
+  var formData = {zipInput: zipInput, 
+                  phoneInput: phoneInput
+                 };
+  var formJSON = JSON.stringify(formData);
+  console.log(formJSON);
+  return formJSON;
+  }
+}
+);
