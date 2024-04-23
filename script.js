de245c65e1d7ad367c3224ffa702691a5911be68
joshjuanzo <164358064+joshjuanzo@users.noreply.github.com>
2024-04-23 18:19:16 +0000

    updated java script

--- script.js
+++ script.js
@@ -48,6 +48,19 @@ document.getElementById("signup-form").addEventListener("submit", function(event
   var formData = {zipInput: zipInput, 
                   phoneInput: phoneInput
                  };
+
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyB4bQHGKOnq-IjW4r3ooYlnqD7j4Sie3rY",
+      authDomain: "aiwebsite-1cdfa.firebaseapp.com",
+      projectId: "aiwebsite-1cdfa",
+      storageBucket: "aiwebsite-1cdfa.appspot.com",
+      messagingSenderId: "434561316894",
+      appId: "1:434561316894:web:f33f53f73c21777fd8960f"
+    };
+
+  const app = initializeApp(firebaseConfig);
   var formJSON = JSON.stringify(formData);
   console.log(formJSON);
   return formJSON;
